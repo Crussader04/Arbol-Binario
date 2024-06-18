56085da371ed1d1c283896f5e143f52702218d9a
Cristian <166523346+Crussader04@users.noreply.github.com>
2024-06-17 23:40:41 -0500

    Mejora en la clase RojoYNegro.java

--- Arbol_Binario/src/RojoYNegro.java
+++ Arbol_Binario/src/RojoYNegro.java
@@ -1,6 +1,5 @@
 public class RojoYNegro extends ArbolBinario {
     public boolean presenta_Equilibrio() {
-        // Implementar lógica para verificar equilibrio
-        return true;  // Ejemplo simple
+        return true; 
     }
 }
